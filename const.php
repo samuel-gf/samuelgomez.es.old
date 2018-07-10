1ee3150685dbd3206fe022283e5873a981d54da2
Samuel Gómez <samuel@ingetec.com.es>
2018-07-10 18:55:47 +0200

    Adaptado const.php para que emplee la contraseña del MAC o del servidor de 1and1 según el caso

--- const.php
+++ const.php
@@ -1,5 +1,12 @@
 <?php
-    define('DATABASE_PASSWORD','uT7A69uJyXPdvBl');
-    define('DATABASE_USER','root');
-    define('DATABASE_HOST','localhost');
-    define('DATABASE_NAME','GenioDelMal');
+    if ($_SERVER['REMOTE_ADDR'] == '192.168.1.10'){
+        define('DATABASE_PASSWORD','uT7A69uJyXPdvBl');
+        define('DATABASE_USER','root');
+        define('DATABASE_HOST','localhost');
+        define('DATABASE_NAME','GenioDelMal');
+    } else {
+        define('DATABASE_PASSWORD','RhL-5aq-vgK-YRn');
+        define('DATABASE_USER','dbo685183673');
+        define('DATABASE_HOST','db685183673.db.1and1.com');
+        define('DATABASE_NAME','db685183673');
+    }
