02a928f6933095c24ce8b38cbaba86a626b908c6
Samuel Gómez <profesor_s@outlook.com>
2019-03-28 18:55:21 +0100

    índice principal es un listado de títulos, no un listado de contenidos

--- templates/header.php
+++ templates/header.php
@@ -16,7 +16,7 @@
 	<link rel="apple-touch-icon" sizes="180x180" href="{{BASE_DIR}}img/favicon-180x180.png" />
 	<link rel="apple-touch-icon" sizes="152x152" href="{{BASE_DIR}}img/favicon-152x152.png" />
 	<link rel="apple-touch-icon" sizes="167x167" href="{{BASE_DIR}}img/favicon-167x167.png" />
-
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600" rel="stylesheet"> 
 
 	<script src='{{BASE_DIR}}js/jquery.js'></script>
 	<script src='{{BASE_DIR}}js/general.js'></script>
